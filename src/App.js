d2cada3af3061f96304e1b67bfcd553315de65f2
luccasrojas <l.rojasb@uniandes.edu.co>
2023-01-15 11:58:53 -0500

    getAvailableRooms implemented in class sobrecupo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import { isElementOfType } from 'react-dom/test-utils';
 
 class Sobrecupo
 {
@@ -13,7 +12,7 @@ class Sobrecupo
     console.log("Extracting info from the api ")
     try
     {
-    this.initalize();
+    this.initalize()
     }
     catch (error)
     {
@@ -24,10 +23,10 @@ class Sobrecupo
   async initalize()
   {
     // const response = await fetch("https://ofertadecursos.uniandes.edu.co/api/courses?term=&ptrm=&prefix=&attr=&nameInput=&campus=CAMPUS%20PRINCIPAL&attrs=&timeStart=&offset=&limit=1")
-    const response = await fetch("https://ofertadecursos.uniandes.edu.co/api/courses?term=&ptrm=&prefix=&attr=&nameInput=&campus=CAMPUS%20PRINCIPAL&attrs=&timeStart=&offset=0&limit=2")
+    const response = await fetch("https://ofertadecursos.uniandes.edu.co/api/courses?term=&ptrm=&prefix=&attr=&nameInput=&campus=CAMPUS%20PRINCIPAL&attrs=&timeStart=&offset=0&limit=300")
     this.response = await response.json()
-    console.log(this.response)
     this.loaded = true;
+    //TODO
     let actual_date = new Date("2023-03-03")
     for (let element of this.response )
     {
@@ -37,13 +36,14 @@ class Sobrecupo
         let date_fin = new Date(pattern.date_fin)
         if (date_ini <= actual_date && date_fin >= actual_date)
         {
+
           //TODO
           // let clasroom = pattern.clasroom
           // let building_name = clasroom.split(" ")[0]
           // let room_name = clasroom.split(" ")[1]
           let building_name = element.class
-          let room_name = element.course
-          console.log(this.buildings[building_name] == null)
+          let room_name = element.course.slice(0,2)
+
           if (this.buildings[building_name] == null)
           {
             this.buildings[building_name] = new Building(building_name)
@@ -65,7 +65,73 @@ class Sobrecupo
       } 
     }
     console.log("All classes were loaded correctly")
-    console.log(this.buildings)
+    console.log(this.getAvailableRooms(0, "06:30","ADMI",3))
+  }
+
+  getAvailableRooms(day, hour, building=undefined, floor=undefined)
+  {
+    let available_rooms = {}
+    var minimum_time = new Date("01/01/2022 23:59:59")
+    if (this.buildings[building] != null)
+    {
+      for (let room_name in this.buildings[building].rooms)
+      {
+        //Revisar si el piso es el correcto en caso dado que haya piso
+        if(floor !== undefined && room_name.slice(0,1) != floor){continue}
+        var room = this.buildings[building].rooms[room_name]
+        minimum_time = new Date("01/01/2022 23:59:59")
+        for (let availability of room.availability[day])
+        {
+          const actual_time = new Date("01/01/2022 "+hour+":00")
+          const class_time_ini = new Date("01/01/2022 "+availability[0]+":00")
+          const class_time_fin = new Date("01/01/2022 "+availability[1]+":00")
+          if (actual_time >= class_time_ini && actual_time <=class_time_fin)
+          {
+            minimum_time = new Date("01/01/2022 23:59:59")
+            break
+          }
+          if (actual_time < class_time_ini && class_time_ini <= minimum_time)
+          {
+            minimum_time = class_time_ini
+          }
+        }
+        if (minimum_time.toString() !== (new Date("01/01/2022 23:59:59")).toString())
+        {
+          available_rooms[building+room_name] = minimum_time
+        }
+      }
+    }
+    else
+    {
+      for (let building_name in this.buildings)
+      {
+        for (let room_name in this.buildings[building_name].rooms)
+        {
+          const room = this.buildings[building_name].rooms[room_name]
+          minimum_time = new Date("01/01/2022 23:59:59")
+          for (let availability of room.availability[day])
+          {
+            const actual_time = new Date("01/01/2022 "+hour+":00")
+            const class_time_ini = new Date("01/01/2022 "+availability[0]+":00")
+            const class_time_fin = new Date("01/01/2022 "+availability[1]+":00")
+            if (actual_time >= class_time_ini && actual_time <=class_time_fin)
+            {
+              minimum_time = new Date("01/01/2022 23:59:59")
+              break
+            }
+            if (actual_time < class_time_ini && class_time_ini <= minimum_time)
+            {
+              minimum_time = class_time_ini
+            }
+          }
+          if (minimum_time.toString() !== (new Date("01/01/2022 23:59:59")).toString())
+          {
+            available_rooms[building_name+room_name] = minimum_time
+          }
+        }
+      }
+    }
+    return available_rooms
   }
 }
 
@@ -103,11 +169,10 @@ class Room
 
   addAvailability(day, availability) 
   {
-    console.log(day)
-    console.log(availability)
     let auxAvailability = [availability[0].slice(0, 2) + ":" + availability[0].slice(2) , availability[1].slice(0, 2) + ":" + availability[1].slice(2)]
     this.availability[day].push(auxAvailability);
   }
+
 }
 
 function App() {
